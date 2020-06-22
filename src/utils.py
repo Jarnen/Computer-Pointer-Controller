a95c90042f27d29a71bdd2ab1d614e8c8ab53a5b
Jarnen Richard <richard.jarnen@gmail.com>
2020-06-22 16:31:42 +0800

    refector: file utils added
    The python file added with functions to be used to draw axes onto the frame. The file has two
    main functions, build camera matrix and draw axes, and more will be added for visualising outputs

---
+++ src/utils.py
@@ -0,0 +1,78 @@
+
+import math
+import numpy as np
+import cv2
+
+def draw_axes(frame, center_of_face, yaw, pitch, roll, scale, focal_length):
+    yaw *= np.pi / 180.0
+    pitch *= np.pi / 180.0
+    roll *= np.pi / 180.0
+    cx = int(center_of_face[0])
+    cy = int(center_of_face[1])
+    Rx = np.array([[1, 0, 0],
+                   [0, math.cos(pitch), -math.sin(pitch)],
+                   [0, math.sin(pitch), math.cos(pitch)]])
+    Ry = np.array([[math.cos(yaw), 0, -math.sin(yaw)],
+                   [0, 1, 0],
+                   [math.sin(yaw), 0, math.cos(yaw)]])
+    Rz = np.array([[math.cos(roll), -math.sin(roll), 0],
+                   [math.sin(roll), math.cos(roll), 0],
+                   [0, 0, 1]])
+    # R = np.dot(Rz, Ry, Rx)
+    # ref: https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+    # R = np.dot(Rz, np.dot(Ry, Rx))
+    R = Rz @ Ry @ Rx
+    
+    camera_matrix = build_camera_matrix(center_of_face, focal_length)
+    xaxis = np.array(([1 * scale, 0, 0]), dtype='float32').reshape(3, 1)
+    yaxis = np.array(([0, -1 * scale, 0]), dtype='float32').reshape(3, 1)
+    zaxis = np.array(([0, 0, -1 * scale]), dtype='float32').reshape(3, 1)
+    zaxis1 = np.array(([0, 0, 1 * scale]), dtype='float32').reshape(3, 1)
+
+    o = np.array(([0, 0, 0]), dtype='float32').reshape(3, 1)
+    o[2] = camera_matrix[0][0]
+
+    xaxis = np.dot(R, xaxis) + o
+    yaxis = np.dot(R, yaxis) + o
+    zaxis = np.dot(R, zaxis) + o
+    zaxis1 = np.dot(R, zaxis1) + o
+    xp2 = (xaxis[0] / xaxis[2] * camera_matrix[0][0]) + cx
+    yp2 = (xaxis[1] / xaxis[2] * camera_matrix[1][1]) + cy
+    p2 = (int(xp2), int(yp2))
+    cv2.line(frame, (cx, cy), p2, (0, 0, 255), 2)
+
+    xp2 = (yaxis[0] / yaxis[2] * camera_matrix[0][0]) + cx
+    yp2 = (yaxis[1] / yaxis[2] * camera_matrix[1][1]) + cy
+    p2 = (int(xp2), int(yp2))
+    cv2.line(frame, (cx, cy), p2, (0, 255, 0), 2)
+
+    xp1 = (zaxis1[0] / zaxis1[2] * camera_matrix[0][0]) + cx
+    yp1 = (zaxis1[1] / zaxis1[2] * camera_matrix[1][1]) + cy
+    p1 = (int(xp1), int(yp1))
+
+    xp2 = (zaxis[0] / zaxis[2] * camera_matrix[0][0]) + cx
+    yp2 = (zaxis[1] / zaxis[2] * camera_matrix[1][1]) + cy
+    p2 = (int(xp2), int(yp2))
+    cv2.line(frame, p1, p2, (255, 0, 0), 2)
+    
+    cv2.circle(frame, p2, 3, (255, 0, 0), 2)
+
+    return frame
+
+def build_camera_matrix(camera_center, focal_length):
+    """
+    Returns camera projection matrix
+
+    Args
+    ------------------
+        camera_center:  tuple (x,y) of the center coordinates
+        focal_lenght:   approximate focal length of the camera in mm
+    """
+    f = focal_length
+    cx = camera_center[0]
+    cy = camera_center[1]
+
+    matrix = np.array([[f, 0, cx],
+                       [0, f, cy],
+                       [0, 0, 1]])
+    return matrix
