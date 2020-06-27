af2c9a245cdb36621370ebee521ddc9aa244ce87
Jarnen Richard <richard.jarnen@gmail.com>
2020-06-27 23:38:49 +0800

    refactor: Updated args and process method
    Remove some args and updated process method to return x.y coordinates and fed to pyautogui.moveto

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ import logging as log
 import sys
 import cv2
 import numpy as np
+from input_feeder import InputFeeder
+from mouse_controller import MouseController
+from inference import Model
+from face_detection import FaceDetection
+from head_pose_estimation import HeadPoseEstimation
+from landmarks_detection import LandmarksDetection
+from gaze_estimation import GazeEstimation
 
 DEVICE_TYPES = ['CPU', 'GPU', 'FPGA', 'MYRAID', 'HETERO']
 
@@ -15,52 +22,31 @@ def build_argparser():
     """
     parser = ArgumentParser()
 
-    general = parser.add_argument_group('General') 
-    general.add_argument('-i', '--input', metavar='PATH', default='0',
+    parser.add_argument('-i', '--input', metavar='PATH', default='cam',
                                   help="(optional) Path to the input video " \
-                                      "('0' for camera, default)")
-    general.add_argument('-o', '--output', metavar="PATH", default="",
-                         help="(optional) Path to save the output video to")
-    
-    models = parser.add_argument_group('Models')
-    models.add_argument('-m_fd', metavar="PATH", default="", required=True,
+                                      "('cam' for camera, default)")
+    parser.add_argument('-f', '--input_file', metavar='PATH', default=None,
+                                  help="(optional) Path to the input file " \
+                                      "(None for input file, default)")
+    parser.add_argument('-m_fd', metavar="PATH", default="", required=True,
                         help="Path to the Face Detection model XML file")
-    models.add_argument('-m_ld', metavar="PATH", default="", required=True,
+    parser.add_argument('-m_ld', metavar="PATH", default="", required=True,
                         help="Path to the Landmarks Detection model XML file")
-    models.add_argument('-m_pe', metavar="PATH", default="", required=True,
+    parser.add_argument('-m_pe', metavar="PATH", default="", required=True,
                         help="Path to the Pose Estimation model XML file")
-    models.add_argument('-m_ge', metavar="PATH", default="", required=True,
+    parser.add_argument('-m_ge', metavar="PATH", default="", required=True,
                         help="Path to the Gaze Estimation model XML file")
     
-    infer = parser.add_argument_group('Inference options')
-    infer.add_argument('-d_fd', default='CPU', choices=DEVICE_TYPES,
+    parser.add_argument('-d', '--device', default='CPU', choices=DEVICE_TYPES,
                        help="(optional) Target device for the " \
                        "Face Detection model (default: %(default)s)")
-    infer.add_argument('-d_ld', default='CPU', choices=DEVICE_TYPES,
-                       help="(optional) Target device for the " \
-                       "Landmarks Detection model (default: %(default)s)")
-    infer.add_argument('-d_pe', default='CPU', choices=DEVICE_TYPES,
-                       help="(optional) Target device for the " \
-                       "Pose Estimation model (default: %(default)s)")
-    infer.add_argument('-d_ge', default='CPU', choices=DEVICE_TYPES,
-                       help="(optional) Target device for the " \
-                       "Gaze Estimation model (default: %(default)s)")
-    infer.add_argument('-l', '--cpu_lib', metavar="PATH", default="",
-                       help="(optional) For MKLDNN (CPU)-targeted custom layers, if any. " \
+    parser.add_argument('-l', '--lib', metavar="PATH", default="",
+                       help="(optional) For targeted device custom layers, if any. " \
                        "Path to a shared library with custom layers implementations")
-    infer.add_argument('-c', '--gpu_lib', metavar="PATH", default="",
-                       help="(optional) For clDNN (GPU)-targeted custom layers, if any. " \
-                       "Path to the XML file with descriptions of the kernels")
-    infer.add_argument('-t_fd', metavar='[0..1]', type=float, default=0.6,
+    parser.add_argument('-t','--threshold', metavar='[0..1]', type=float, default=0.6,
                        help="(optional) Probability threshold for face detections" \
                        "(default: %(default)s)")
-    infer.add_argument('-t_ld', metavar='[0..1]', type=float, default=0.6, 
-                       help="(optional) Probability threshold for landmarks detections" \
-                       "(default: %(default)s)")
-    infer.add_argument('-exp_r_fd', metavar='NUMBER', type=float, default=1.15,
-                       help="(optional) Scaling ratio for bboxes passed to face recognition " \
-                       "(default: %(default)s)")
-    infer.add_argument('-v', '--verbose', action='store_true',
+    parser.add_argument('-v', '--verbose', action='store_true',
                        help="(optional) Be more verbose")
     
     return parser
@@ -68,17 +54,46 @@ def build_argparser():
 class ProcessFrame:
 
     def __init__(self, args):
-        devices = set([args.d])
+        self.face_detector = FaceDetection(args.m_fd, args.device, args.lib, args.threshold)
+        self.landmarks_detector = LandmarksDetection(args.m_ld, args.device, args.lib, args.threshold)
+        self.head_pose_estimation = HeadPoseEstimation(args.m_pe, args.device, args.threshold)
+        self.gaze_estimation = GazeEstimation(args.m_ge,args.device, args.threshold)
+        self.mouse_controller = MouseController(0.5, 0.5)
+
+        self.face_detector.load_model()
+        self.landmarks_detector.load_model()
+        self.head_pose_estimation.load_model()
+
+    def process(self, frame):
+        assert len(frame.shape) == 3, "Expected input frame in (H, W, C) format"
+        assert frame.shape[2] in [3, 4], "Expected BGR or BGRA input"
+
+        rois = self.face_detector.predict(frame)
+        face = self.face_detector.preprocess_output(frame, rois)
+        
+        landmarks = self.landmarks_detector.predict(face)
+        right_eye_image, left_eye_image = self.landmarks_detector.preprocess_output(face, landmarks)
+
+        head_pose_angles = self.head_pose_estimation.predict(face)
+        
+        gaze_estimation_inputs = self.gaze_estimation.preprocess_input(left_eye_image, right_eye_image, head_pose_angles)
+        gaze_result = self.gaze_estimation.predict(gaze_estimation_inputs)
+        x, y = self.gaze_estimation.preprocess_output(gaze_result)
+
+        self.mouse_controller.move(x,y)
+
+
 class Visualize:
     BREAK_KEY_LABELS = "q(Q) or Escape"
     BREAK_KEYS = {ord('q'), ord('Q'), 27}
 
-    def __init__(self):
+    def __init__(self, args):
         """
         Initialise the variables
         """
-    
-    def frame_detector(frame):
+        self.process_frame = ProcessFrame(args)
+
+    def frame_detector(self, frame):
         """
         Detects and returns face coordinates on the frame
         """
@@ -98,7 +113,8 @@ class Visualize:
         """
         Draw yaw, pitch, roll angle lines for visualisation
         """
-        draw_axes(frame, center, yaw, pitch, roll, focal_length = 50)
+        scale = 2
+        draw_axes(frame, center, yaw, pitch, roll, scale, focal_length = 50)
     
     def display_window(self, frame):
         color = (255 ,255 ,255 )
@@ -109,50 +125,18 @@ class Visualize:
         text_size = cv2.getTextSize(text, font, text_scale, thickness)
         origin = np.array([frame.shape[-2] - text_size[0][0] - 10, 10])        
         cv2.putText(frame, text, tuple(origin.astype(int)), font, text_scale, color, thickness)
-
-    def process(self, input_stream):
-        self.input_stream = input_stream
-
-        while input_stream.isOpened():
-            ret_val, frame = input_stream.read()
-            if not ret_val:
-                break
-
-            face_detection = self.frame_detector(frame)
-
-
-    
-    @staticmethod
-    def open_input_stream(path):
-        log.info("Reading input data from '%s'" % (path))
-        stream = path
-        try:
-            stream = int(path)
-        except ValueError:
-            pass
-        return cv2.VideoCapture(stream)
         
     def run(self, args):
-        input_stream = Visualize.open_input_stream(args.input)
-        if input_stream is None or not input_stream.isOpened():
-            log.error("Cannot open input stream: %s" % args.input)
-        fps = input_stream.get(cv2.CAP_PROP_FPS)
-        frame_size = (int(input_stream.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                      int(input_stream.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-        self.frame_count = int(input_stream.get(cv2.CAP_PROP_FRAME_COUNT))
-        log.info("Input stream info: %d x %d @ %.2f FPS" % \
-            (frame_size[0], frame_size[1], fps))
-        
-        self.process(input_stream)
-
+        feed = InputFeeder(args.input_type, args.input_file)
+        feed.load_data()
+        for batch in feed.next_batch():
+            self.process_frame.process(batch)
+        feed.close
         # Release resources
-        if input_stream:
-            input_stream.release()
-
         cv2.destroyAllWindows()
 
 def main():
-    args = build_argparser().parse_args
+    args = build_argparser().parse_args()
 
     log.basicConfig(format="[ %(levelname)s ] %(asctime) - 15s %(message)s ",
                     level=log.INFO if not args.verbose else log.DEBUG, stream=sys.stdout)
