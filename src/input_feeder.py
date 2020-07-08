118badcc3339fea79cb6c738542779d00d500577
Jarnen Richard <richard.jarnen@gmail.com>
2020-07-08 10:54:12 +0800

    refactor: created batch size arg

--- src/input_feeder.py
+++ src/input_feeder.py
@@ -13,12 +13,14 @@ from numpy import hstack
 import logging as log
 
 class InputFeeder:
-    def __init__(self, input_type, input_file=None):
+    def __init__(self, input_type, input_file, batch_size):
         '''
         input_type: str, The type of input. Can be 'video' for video file, 'image' for image file,
                     or 'cam' to use webcam feed.
         input_file: str, The file that contains the input image or video file. Leave empty for cam input_type.
         '''
+        self.batch_size = batch_size
+
         self.input_type=input_type
         if input_type=='video' or input_type=='image':
             self.input_file=input_file
@@ -37,10 +39,11 @@ class InputFeeder:
         If input_type is 'image', then it returns the same image.
         # '''
         while True:
-            for retVal in range(2):
+            for retVal in range(self.batch_size):
                 retVal, frame=self.cap.read()
 
                 if self.input_type=='cam':
+                    # mirror the frame
                     frame = cv2.flip(frame,1)
                 
                 if not retVal:
